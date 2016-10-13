9427de17a68114907426f76efd0b6bec963cc8a8
Sirichai Saeaiew <sirichai@365zocial.com>
2016-10-13 17:34:21 +0700

    Update Bot

--- bot.php
+++ bot.php
@@ -19,15 +19,20 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+                'type' => 'text',
+    			'text' => $text
+			];
+
+            $messages2 = [
+                'type' => 'text',
+    			'text' => 'ทดสอบอยู่ใจล่ม ๆ ครับ'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages, $messages2],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
